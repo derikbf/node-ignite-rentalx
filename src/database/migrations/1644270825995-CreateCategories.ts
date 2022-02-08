f824bf0999da4761c73ad16594985fdec5c2fb66
derikbf <derikb@gmail.com>
2022-02-07 21:06:42 -0300

    Config database postgresql

---
+++ src/database/migrations/1644270825995-CreateCategories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategories1644270825995 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
