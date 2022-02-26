743a960aa3eeba85944e324ee5912013307e675f
derikbf <derikb@gmail.com>
2022-02-26 10:44:59 -0300

    Updated server with container

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { router } from './routes';
 import swaggerUi from "swagger-ui-express";
-import swaggerFile  from "./swagger.json";
 
 import "./database";
+
+import "./shared/container"
+
+import { router } from './routes';
+import swaggerFile  from "./swagger.json";
 
 const app = express();
 
