352a5985df9bba205af85bb013acded4f2edecae
derikbf <derikb@gmail.com>
2022-02-26 10:50:13 -0300

    Updated imp categories use case with injectable

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,6 @@
-import csvParse from "csv-parse";
+import { parse } from "csv-parse";
 import fs from "fs";
+import { inject, injectable } from 'tsyringe';
 import { ICategoriesRepository } from "../../../../modules/cars/repositories/ICategoriesRepository"
 
 interface IImportCategory{
@@ -7,9 +8,13 @@ interface IImportCategory{
   description: string;
 }
 
+@injectable()
 class ImportCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
-  
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository,
+  ) { }
+
   loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path);
@@ -34,16 +39,16 @@ class ImportCategoryUseCase {
         })
     })
   }
-  
+
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    
-    categories.map(async (category) => {
+
+    categories.map(category => {
       const { name, description } = category;
 
       const existCategory = this.categoriesRepository.findByName(name);
 
-      if(!existCategory) {
+      if (!existCategory) {
         this.categoriesRepository.create({
           name,
           description,
