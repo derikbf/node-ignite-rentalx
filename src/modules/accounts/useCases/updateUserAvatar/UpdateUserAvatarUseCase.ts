62624d7460341a82c4377a088906cf05c6123141
derikbf <derikb@gmail.com>
2022-02-28 08:54:18 -0300

    Created Migration User Avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,13 @@
+// add coluna avatar na tabela de user
+// refatorar user com coluna avatar
+// configuração upload multer
+// criar regra de negocio do upload
+// criar controller
+
+class UpdateUserAvatarUseCase {
+  async execute() {
+    
+  }
+}
+
+export { UpdateUserAvatarUseCase }
